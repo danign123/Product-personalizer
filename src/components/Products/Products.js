79a32809b692e4711fb973bde9e793a52e23563c
Daniel Ignaciuk <daniel.ignaciuk@gmail.com>
2022-02-17 18:09:34 +0100

    First commit

---
+++ src/components/Products/Products.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import productsData from '../../data/products';
+import Product from '../Product/Product';
+
+const Products = () => {
+  const [products]  = useState(productsData);
+
+  return (
+    <section>
+
+      {products.map(product => <Product key={product.id} id={product.id}  name={product.name}  title={product.title} colors={product.colors} sizes={product.sizes} basePrice={product.basePrice} />)}
+
+      {/* <Product
+        id={products[0].id}
+        name={products[0].name}
+        title={products[0].title}
+        colors={products[0].colors}
+        sizes={products[0].sizes}
+        basePrice={products[0].basePrice} />
+      <Product
+        id={products[1].id}
+        name={products[1].name}
+        title={products[1].title}
+        colors={products[1].colors}
+        sizes={products[1].sizes}
+        basePrice={products[1].basePrice} /> */}
+    </section>
+  );
+};
+
+export default Products;
