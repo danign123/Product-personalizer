c50e614a1499f20323dd567edf7bfb0b11e2850d
Daniel Ignaciuk <daniel.ignaciuk@gmail.com>
2022-02-22 17:15:39 +0100

    Finish all components

---
+++ src/components/Product/ProductForm.js
@@ -0,0 +1,34 @@
+
+import OptionSize from "../OptionSize/OptionSize"
+import OptionColor from "../OptionColor/OptionColor"
+import Button from "../Button/Button"
+import styles from './ProductForm.module.scss';
+import PropTypes from 'prop-types'
+
+const ProductForm = ({ handleSubmit, sizes, colors, currentSize, setCurrentSize, currentColor, setCurrentColor}) => {
+
+    return (
+        <form onSubmit={handleSubmit}>
+            
+            <OptionSize sizes={sizes} currentSize={currentSize} setCurrentSize={setCurrentSize}/>
+
+            <OptionColor colors={colors} currentColor={currentColor} setCurrentColor={setCurrentColor}/>
+
+            <Button className={styles.button} >
+            <span className="fa fa-shopping-cart" />
+            </Button>
+        </form>
+    )
+}
+
+ProductForm.propTypes ={
+  handleSubmit: PropTypes.func.isRequired,
+  colors: PropTypes.array.isRequired,
+  sizes: PropTypes.array.isRequired,
+  currentColor: PropTypes.string.isRequired,
+  currentSize: PropTypes.string.isRequired,
+  setCurrentColor: PropTypes.func.isRequired,
+  setCurrentSize: PropTypes.func.isRequired,
+};
+
+export default ProductForm
